d75441d4a8829f8701ab617b8e3ac77d0abc782e
JuniorOliveira323 <aldenirabreu869@gmail.com>
2026-06-02 12:00:19 -0300

    Instalação do Router Dom + Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import Login from './Components/Login/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App(){
-  return(
-    <div>
-      <Login/>
-    </div>
-  )
+import Login from './Components/Login/Login';
+import TelaPrincipal from './Components/TelaPrincipal/TelaPrincipal';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<TelaPrincipal />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
